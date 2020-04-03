21befb64800cdbe9d65360eafbb6c36dfbe34ca4
Allison Mackey <a.mackey693@gmail.com>
2020-04-03 15:52:05 -0700

    creates li output in jQuery UI & HTML

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,31 @@
 //Business Logic : 
 function roboConversion (userInput) {
   var counts = []; // creates array for userInput 
-  var removeItems = [1, "Boop"]
-  var newString = [" "];
+  var removeItems = [3, 2, 1];
+  var replaceItems = ["WYBMN", "Boop", "Beep"]
+  var newArray = [];
   
   if (userInput === 0) {
     alert('Enter Number');
   }  
   // creates new array for counting numbers
   for (var i = 0; i <= userInput; i += 1) {  
-    counts.push(i);
-  } if (counts.length <= 1) {
-    counts.pop("Beep");
-  }
-    console.log(counts)
-    // console.log(removeItems)
-    // console.log(newString)
+    counts.push(i) 
+  } if (counts === removeItems) {
+    counts.replaceItems; 
+  } 
+  console.log(counts)
+  return counts
+  
+
+      // I don't know what I'm doing & it's frustrating me!!!
+
+  // } if (counts.indexOf(1)||counts.indexOf(2)||counts.indexOf(3)) {
+  //   counts.pop("Beep");
+  // } 
+  //  
+  // console.log(removeItems)
+  // console.log(newString)
 }
 
 
@@ -35,9 +45,10 @@ $(document).ready(function(){
     var userInput = $("#userInput").val().split();
     var newOutput = roboConversion(parseInt(userInput));
     
-    // console.log(userInput);
-    // console.log(newOutput);
+    console.log("UserInput", userInput);
+    console.log("newOutput", newOutput);
+    
 
-    $("#output").append(newOutput)
+    $("#output").html("<li>" + newOutput + "</li>")
   });
 });
