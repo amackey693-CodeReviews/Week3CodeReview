76da8ba26bb4948725f45e15877e35d06ce0836c
Allison Mackey <a.mackey693@gmail.com>
2020-04-03 15:22:42 -0700

    attempts to re-factor code to implement 3rd behavior.

--- js/scripts.js
+++ js/scripts.js
@@ -1,50 +1,43 @@
 //Business Logic : 
-
-
 function roboConversion (userInput) {
-  var removeItems = [1]
-  var replaceItems = ["Boop"]
-  var newString = [" "];
   var counts = []; // creates array for userInput 
-
+  var removeItems = [1, "Boop"]
+  var newString = [" "];
+  
   if (userInput === 0) {
     alert('Enter Number');
   }  
   // creates new array for counting numbers
   for (var i = 0; i <= userInput; i += 1) {  
     counts.push(i);
-  } counts.forEach(function(num) {
-    if (num === removeItems) {
-      num.replace(replaceItems);
-      newString.push(num)
-    }
+  } if (counts.length <= 1) {
+    counts.pop("Beep");
+  }
     console.log(counts)
-    console.log(removeItems)
-   });
-  return newString
+    // console.log(removeItems)
+    // console.log(newString)
 }
 
 
-// attempts to 
-//   } if (counts.length === removeItems) {
-// //     for (var i = counts; i <= counts; i++) {
-// //       newString = counts.push(i + replaceItems)
-// //     }
-// //   }
-// //   return newString;
-// // }
+  // attempts to 
+  //   } if (counts.length === removeItems) {
+  // //     for (var i = counts; i <= counts; i++) {
+  // //       newString = counts.push(i + replaceItems)
+  // //     }
+  // //   }
+  // //   return newString;
+  // // }
 
-//User Interface Logic: 
+  //User Interface Logic: 
 $(document).ready(function(){
   $("form").submit(function(event){
     event.preventDefault();
-    var userInput = parseInt($("#userInput").val().split())
-    var newOutput = roboConversion(userInput);
+    var userInput = $("#userInput").val().split();
+    var newOutput = roboConversion(parseInt(userInput));
     
     // console.log(userInput);
     // console.log(newOutput);
 
-   
     $("#output").append(newOutput)
   });
 });
