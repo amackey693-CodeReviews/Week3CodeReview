a193b8cf74507240665ae0f246240cc52ed283e1
Allison Mackey <a.mackey693@gmail.com>
2020-04-03 12:41:03 -0700

    stuck on second spec, cannot get behavior to work. will keep trying.

--- js/scripts.js
+++ js/scripts.js
@@ -1,50 +1,29 @@
 //Business Logic : 
-var counts = [""];
+var counts = [];
 var roboConversion = function (userInput) {
   if (userInput === 0) {
     return alert('Enter Number');
   }  
-  for (var i = 0; i <= userInput; i ++) {
-    var counting = counts.push(i);
+  for (var i = 0; i <= userInput; i += 1) {
+    var counting = [counts.push(i)];
     console.log("counting", counting)
-    if (i===1){
-      counts.push("Beep!");
-      
-  } 
+  }
 }
-//   var userInput = num.split(" ");
-//   var newOutput = userInput.forEach(function(num1) {
-//     if (num1.includes("1")) {
-//       return num1.replace("Beep");
-//     }
-//     console.log(userInput)
-//     console.log(newOutput)
-//     return newOutput
-//   });
-// } 
-
-
-//   if (num.indexOf("1")) {
-//     userInput.forEach(function(input) {
-//       newOutput.push(input + "Beep")
-//     });
-//   }
-// }
-
-
-
-
 
 //User Interface Logic: 
 $(document).ready(function(){
   $("form").submit(function(event){
     event.preventDefault();
     var userInput = parseInt($("#userInput").val())
-    var newOutput = roboConversion(userInput);
+    var newOutput = [roboConversion(userInput)];
+    console.log(userInput);
+    console.log(newOutput);
 
-    
-    
-
-    $("#output").show(newOutput)
+    newOutput.forEach(num => {
+      if (num === 1) {
+        newOutput.remove(1).push("Beep"); 
+      }
+    });
+    $("#output").append(newOutput)
   });
 });
