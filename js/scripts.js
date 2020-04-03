1286870b6d8941c45f237464eaaf0626e444102c
Allison Mackey <a.mackey693@gmail.com>
2020-04-03 10:31:24 -0700

    first behavior spec passes in console. still need to make visible in UI

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,29 @@
 //Business Logic : 
+var contains = ["1", "2", "3"];
+
+var roboConversion = function (userInput) {
+  if (userInput != NaN || userInput === 0) {
+    return 'Enter Number';
+  }
+}
+//   var userInput = num.split(" ");
+//   var newOutput = userInput.forEach(function(num1) {
+//     if (num1.includes("1")) {
+//       return num1.replace("Beep");
+//     }
+//     console.log(userInput)
+//     console.log(newOutput)
+//     return newOutput
+//   });
+// } 
 
 
+//   if (num.indexOf("1")) {
+//     userInput.forEach(function(input) {
+//       newOutput.push(input + "Beep")
+//     });
+//   }
+// }
 
 
 
@@ -15,5 +38,7 @@ $(document).ready(function(){
 
     console.log(userInput)
     console.log(newOutput)
+
+    $("#output").show(newOutput)
   });
 });
