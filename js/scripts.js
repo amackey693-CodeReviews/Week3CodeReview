682e1d9318584ff40e5eac2f091df6b270076427
Allison Mackey <a.mackey693@gmail.com>
2020-04-12 10:43:27 -0700

    finalizes html and scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,13 @@
 //Business Logic : 
 function roboConversion (userInput) {
-  // var counts = []; // creates a counting array from userInput 
-  var finalResult = []; //final array to push to 
+  var finalResult = []; 
   
-  if (userInput === 0) {
-    alert('Enter Number');
+  if (isNaN(userInput) || userInput === 0) {
+    finalResult.push("Enter Number!");
+    return finalResult;
   }
 
-  // // creates new array for counting numbers
-  // for (var i = 0; i <= userInput; i += 1) {  
-  //   counts.push(i);
-  // }
-
   for (var i = 0; i <= userInput; i += 1) {
-    // var num = counts[i]; // creates variable from counts "at" index 
     var num = i.toString(); // creates string from index of "userInput"
 
     if (num.indexOf("3") > -1 ) {
@@ -44,8 +38,6 @@ $(document).ready(function(){
     var userInput = $("#userInput").val().split();
     var newOutput = roboConversion(parseInt(userInput));
     
-    // console.log("newOutput", newOutput);
-
     $("#output").html("<li>" + newOutput + "</li>")
   });
 });
