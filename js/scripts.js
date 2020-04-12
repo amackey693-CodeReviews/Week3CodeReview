51c2fb34d58c339d17d4b7fe8bac4b41bb6d703c
Allison Mackey <a.mackey693@gmail.com>
2020-04-12 10:21:23 -0700

    adds argument to if statement that corrects repeating sentence 'won't you be my neighbor', when .indexOf() method was added it was throwing -1's out every time it was searching the index

--- js/scripts.js
+++ js/scripts.js
@@ -1,36 +1,37 @@
 //Business Logic : 
 function roboConversion (userInput) {
-  var counts = []; // creates a counting array from userInput 
+  // var counts = []; // creates a counting array from userInput 
   var finalResult = []; //final array to push to 
   
   if (userInput === 0) {
     alert('Enter Number');
   }
 
-  // creates new array for counting numbers
-  for (var i = 0; i <= userInput; i += 1) {  
-    counts.push(i);
-  }
+  // // creates new array for counting numbers
+  // for (var i = 0; i <= userInput; i += 1) {  
+  //   counts.push(i);
+  // }
 
-  for (var i = 0; i <= counts.length; i ++) {
+  for (var i = 0; i <= userInput; i += 1) {
     // var num = counts[i]; // creates variable from counts "at" index 
     var num = i.toString();
 
-    if (num === "1") {
-      num = "Beep"
+    if (num.indexOf("3") > -1 ) {
+      num = "Won't you be my neighbor?";
     }
-    if (num === "2") {
-      num = "Boop"
-    }
-    if (num === "3") {
-      num = "Won't you be my neighbor?"
-    }
+    // else if (num.indexOf("2")) {
+    //   num = "Boop"
+    // }
+    // else if (num.indexOf("1")) {
+    //   num = "Beep"
+    // }
     
     finalResult.push(num);
   } 
+  console.log("i", i)
   console.log("num", num);
-  console.log("counts", counts);
-  console.log("final result", finalResult);
+  // console.log("counts", counts);
+  // console.log("final result", finalResult);
   return finalResult;  
 }
 
